c9a2f3c2b917d2fb627b8b43996eee62bc8854e0
Caique Gomes <caiquegomesdev@gmail.com>
2024-05-12 18:05:59 -0300

    Aula 13

---
+++ aula13.py
@@ -0,0 +1,18 @@
+nome = 'Caique Gomes'
+altura = 1.80
+peso = 72
+imc = peso / altura ** 2
+
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+#Formatacao metodo otimizado
+# Caique Gomes tem 1.80 de altura,
+# pesa 72 quilos e seu IMC é
+# 22.22
