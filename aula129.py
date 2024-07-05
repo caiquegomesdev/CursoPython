39e2d2873d342eb483e3149bda3141e8d5e58eda
Caique <caiquegomesdev@gmail.com>
2024-07-05 05:06:23 -0300

    Aula 129

--- aula129.py
+++ aula129.py
@@ -2,7 +2,7 @@
 # Métodos estáticos são métodos que estão dentro da
 # classe, mas não tem acesso ao self nem ao cls.
 # Em resumo, são funções que existem dentro da sua
-# classe.
+# classe..
 class Classe:
     @staticmethod
     def funcao_que_esta_na_classe(*args, **kwargs):
