4135d310d5b7eb0abd26f9b56f4bd2c028bb2361
Caique <caiquegomesdev@gmail.com>
2024-07-07 05:09:50 -0300

    Aula 134

--- aula134.py
+++ aula134.py
@@ -1,12 +1,12 @@
 # Relações entre classes: associação, agregação e composição
-# Associação é um tipo de relação onde os objetos
+# Associação é um tipo de relação onde os objetos.
 # estão ligados dentro do sistema.
 # Essa é a relação mais comum entre objetos e tem subconjuntos
 # como agregação e composição (que veremos depois).
 # Geralmente, temos uma associação quando um objeto tem
 # um atributo que referencia outro objeto.
 # A associação não especifica como um objeto controla
-# o ciclo de vida de outro objeto.
+# o ciclo de vida de outro objeto...
 class Escritor:
     def __init__(self, nome) -> None:
         self.nome = nome
